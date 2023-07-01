37e33af527ee76f5459ee8803d5a6668a708d1cc
Mahdi <mahdi@Mahdis-MacBook-Air.local>
2023-07-01 12:27:52 -0400

    Cleaned Code

--- dmod/dm.js
+++ dmod/dm.js
@@ -1,80 +1,14 @@
 import Web3 from "web3";
-var accounts = {
+const config = require("../config.js");
+
+var activeAgents = {
     "acc1": "free",
     "acc2": "free",
     "acc3": "free"
 };
 
-const abi = [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "givenId",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "givenURL",
-				"type": "string"
-			}
-		],
-		"name": "setURL",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "givenId",
-				"type": "string"
-			}
-		],
-		"name": "getURL",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "url",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"name": "ids",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "url",
-				"type": "string"
-			},
-			{
-				"internalType": "bool",
-				"name": "isValue",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
-
-// For CELO NETWORK
-// const web3 = new Web3("https://alfajores-forno.celo-testnet.org");
-// const DUSContract = new web3.eth.Contract(abi, "0x5f4CE13F55b2698f0Ee402fFcE4805fC0f6748fb");
-
 const web3 = new Web3("https://polygon-rpc.com");
-const DUSContract = new web3.eth.Contract(abi, "0xa4e031Cddf9BF145f331d1A21eB9cC5290452cdc");
+const DUSContract = new web3.eth.Contract(config.CONTRACT_ABI, "0xa4e031Cddf9BF145f331d1A21eB9cC5290452cdc");
 
 /**
  * Returns the URL based on a given id
@@ -99,46 +33,21 @@ async function getURL(id){
  * @return {Promise<string>} The shortened URL's id
  */
 async function shortenURL(url){
-    var id = "N/A";
-
 	var agent = getFreeAgent();
-	accounts[agent] = "locked";
 	if (agent == "N/A"){
 		return "N/A";
 	}
-
-    // Ensure the ID is unique and not taken by another URL in the contract
-    while (id == "N/A"){
-        id = generateId(6);
-        if (await getURL("p" + id) != "N/A"){
-            id = "N/A";
-        }
-    }
+	activeAgents[agent] = "locked";
+	
 	var privateKey = getPrivateFromAgent(agent);
-	console.log(privateKey);
-	console.log(agent);
-	console.log(process.env.ACCOUNT1)
     var acc = web3.eth.accounts.privateKeyToAccount(privateKey);
     web3.eth.accounts.wallet.add(acc);
     web3.eth.defaultAccount = acc.address;
 
-	// const gasPrice = signer.gasPrice();
-	
-	// const gasLimit = DUSContract.estimateGas.setURL(id, url);
-	DUSContract.methods.setURL(id, url).estimateGas(
-        {
-            from: acc.address
-        }, function(error, estimatedGas) {
-			console.log(estimatedGas);
-        }
-    );
-	console.log("SDDD_________");
-	// console.log(gasLimit);
-	console.log(await web3.eth.getGasPrice());
-
+	var id = await generateUniqueId();
     await DUSContract.methods.setURL(id, url).send({from: acc.address, gas: 227079, gasPrice: 194219599216});
 	
-	accounts[agent] = "free";
+	activeAgents[agent] = "free";
 
     return "p" + id;
 }
@@ -148,9 +57,9 @@ async function shortenURL(url){
  * @return {string} Returns the agent account number "acc1" or returns "N/A" meaning there are no free agents.
  */
 function getFreeAgent(){
-	var keys = Object.keys(accounts);
+	var keys = Object.keys(activeAgents);
 	for (var i = 0; i < keys.length; i++){
-		if (accounts[keys[i]] == "free"){
+		if (activeAgents[keys[i]] == "free"){
 			return keys[i];
 		}
 	}
@@ -185,6 +94,17 @@ function generateId(length) {
         id += chars[Math.floor(Math.random() * 62)];
     }
     return id;
+}
+
+async function generateUniqueId(){
+	var id = "N/A";
+	while (id == "N/A"){
+        id = generateId(6);
+        if (await getURL("p" + id) != "N/A"){
+            id = "N/A";
+        }
+    }
+	return id;
 }
 
 module.exports = { getURL, shortenURL };
